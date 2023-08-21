8fcad06f689e6ebf6d1961b3b76deae8a35de7ec
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2023-08-21 09:49:46 +0200

    Create columns for 16x16 grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,17 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 
-for(let counter = 0; counter < 16; counter ++){
+for(let rowCounter = 0; rowCounter < 16; rowCounter ++){
     const gridRow = document.createElement('div');
     gridRow.classList.add('gridRow');
-    gridRow.textContent = 'row for grid';
+    //gridRow.textContent = 'row for grid';
     gridContainer.appendChild(gridRow);
+    for (let columnCounter = 0; columnCounter < 16; columnCounter ++){
+        const gridColumn = document.createElement('div');
+        gridColumn.classList.add('gridColumn');
+        gridColumn.textContent = "column for grid";
+        gridRow.appendChild(gridColumn);
+        
+    }
 
 }
